34a3bbdc8aedeab7c9fd4f139d3601522c481919
José Manuel Galán <devgalan@gmail.com>
2024-05-21 20:03:39 +0200

    Add bottomNavView

--- app/src/main/java/com/devgalan/caloriecalculator/ui/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/devgalan/caloriecalculator/ui/calculator/CalculatorFragment.kt
@@ -33,7 +33,7 @@ class CalculatorFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.btnCalculate.setOnClickListener {
             findNavController().navigate(R.id.action_CalculatorFragment_to_ResultFragment)
         }
     }
--- app/src/main/java/com/devgalan/caloriecalculator/ui/home/MainActivity.kt
+++ app/src/main/java/com/devgalan/caloriecalculator/ui/home/MainActivity.kt
@@ -9,30 +9,32 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.devgalan.caloriecalculator.R
 import com.devgalan.caloriecalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        initUI()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    private fun initUI() {
+        initNavigation()
     }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+
 }
