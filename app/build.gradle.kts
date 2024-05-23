8ddad1be81ac6244bb1c4336245fb6fea5d93cce
José Manuel Galán <devgalan@gmail.com>
2024-05-23 15:34:32 +0200

    New app logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.devgalan.caloriecalculator"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.2.2"
+        versionCode = 6
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
